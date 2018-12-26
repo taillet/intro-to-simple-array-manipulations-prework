25fb09c52c8752a45916c85f3c26fd2cadd25e38
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 17:14:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+
+def using_push
+end
+
+def using_unshift
+end
+
+def using_pop
+end
+
+def pop_with_args
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
+
+def using_concat
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
